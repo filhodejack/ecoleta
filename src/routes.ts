4d2daaa0b6db801f6455879cb7306d91919cf8c0
Jackson Elzo <7309450+filhodejack@users.noreply.github.com>
2020-06-03 23:18:24 -0300

    feat: Items route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,18 @@
 import express from 'express'
+import knex from './database/connection'
 
 const routes = express.Router()
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello, world!' })
+routes.get('/items', async (req, res) => {
+  const url = req.protocol + '://' + req.get('host')
+  const items = await knex('items').select('*')
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `${url}/uploads/${item.image}`,
+    }
+  })
+  return res.json(serializedItems)
 })
 
 export default routes
