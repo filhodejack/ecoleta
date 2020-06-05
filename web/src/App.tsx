721b507e845e17621d3532976dc6a12359f6fd6c
Jackson Elzo <7309450+filhodejack@users.noreply.github.com>
2020-06-05 13:24:12 -0300

    feat: add immutability

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 import Header from './Header'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  
+  function handleButtonClick() {
+    setCounter(counter + 1)
+  }
+
   return (
     <div className="App">
       <Header title="Hello, otherworldly!" />
-      <h1>Conteúdo da aplicação</h1>
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Rise</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
