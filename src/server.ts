4a351bb147312879a6f1e4e69ccaffaaa098548c
Jackson Elzo <7309450+filhodejack@users.noreply.github.com>
2020-06-04 22:47:15 -0300

    feat: cors and static image for points

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
+import cors from 'cors'
 import express, { response, request } from 'express'
 import path from 'path'
 import routes from './routes'
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
 app.listen(3333)
+
 console.log('###############################################')
 console.log('#                                             #')
 console.log('# Server listening on: http://localhost:3333/ #')
