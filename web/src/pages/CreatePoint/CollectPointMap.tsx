a0fde4c45ce8834b5f043439fa7c92b3320c512a
Jackson Elzo <7309450+filhodejack@users.noreply.github.com>
2020-06-06 20:40:23 -0300

    feat: set marker on user clicks on map

--- web/src/pages/CreatePoint/CollectPointMap.tsx
+++ web/src/pages/CreatePoint/CollectPointMap.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import { LeafletMouseEvent } from "leaflet";
+import React, { useState } from "react";
 import { Map, Marker, TileLayer } from "react-leaflet";
 
-const CollectPointMap = () => (
-  <Map center={[-20, -46]} zoom={15}>
-    <TileLayer
-      attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[-20, -46]} />
-  </Map>
-);
+type LatLng = [number, number];
+
+const CollectPointMap = () => {
+  const [selectedPosition, setSelectedPosition] = useState<LatLng>([0, 0]);
+
+  function handleMapClick(event: LeafletMouseEvent) {
+    setSelectedPosition([event.latlng.lat, event.latlng.lng]);
+  }
+
+  return (
+    <Map center={[-20, -46]} zoom={15} onclick={handleMapClick}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={selectedPosition} />
+    </Map>
+  );
+};
 
 export default CollectPointMap;
