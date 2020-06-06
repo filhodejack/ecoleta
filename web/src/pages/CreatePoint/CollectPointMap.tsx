864b7b56266bb890a045c458d1666070cc033c1d
Jackson Elzo <7309450+filhodejack@users.noreply.github.com>
2020-06-06 20:56:11 -0300

    feat: get current user locale and set initial map position

--- web/src/pages/CreatePoint/CollectPointMap.tsx
+++ web/src/pages/CreatePoint/CollectPointMap.tsx
@@ -1,18 +1,26 @@
 import { LeafletMouseEvent } from "leaflet";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Map, Marker, TileLayer } from "react-leaflet";
 
 type LatLng = [number, number];
 
 const CollectPointMap = () => {
+  const [initialPosition, setInitialPosition] = useState<LatLng>([0, 0]);
   const [selectedPosition, setSelectedPosition] = useState<LatLng>([0, 0]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      setInitialPosition([latitude, longitude]);
+    });
+  }, []);
 
   function handleMapClick(event: LeafletMouseEvent) {
     setSelectedPosition([event.latlng.lat, event.latlng.lng]);
   }
 
   return (
-    <Map center={[-20, -46]} zoom={15} onclick={handleMapClick}>
+    <Map center={initialPosition} zoom={15} onclick={handleMapClick}>
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
