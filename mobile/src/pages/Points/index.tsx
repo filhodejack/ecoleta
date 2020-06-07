c4aa31a8af1b9bd278f748c2216dfe2b275b06c1
Jackson Elzo <7309450+filhodejack@users.noreply.github.com>
2020-06-07 15:15:28 -0300

    feat: create map page

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,12 +1,237 @@
+import { Feather as Icon } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import Constants from "expo-constants";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import MapView, { LatLng, Marker, Region } from "react-native-maps";
+import { SvgUri } from "react-native-svg";
 
 const Points = () => {
+  const navigation = useNavigation();
+  const initialCoordinates: LatLng = { latitude: -27.209, longitude: -49.64 };
+  const initialRegion: Region = {
+    latitude: initialCoordinates.latitude,
+    latitudeDelta: 0.014,
+    longitude: initialCoordinates.longitude,
+    longitudeDelta: 0.014,
+  };
+  const imageSource = {
+    uri:
+      "https://media-cdn.tripadvisor.com/media/photo-s/15/d3/c2/93/popular-market.jpg",
+  };
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
+  function handleNavigateToDetail() {
+    navigation.navigate("Detail");
+  }
+
   return (
-    <View>
-      <Text>Maoeeeee</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>😉 Bem vindo.</Text>
+
+        <Text style={styles.description}>
+          Encontre no mapa um ponto de coleta.
+        </Text>
+
+        <View style={styles.mapContainer}>
+          <MapView initialRegion={initialRegion} style={styles.map}>
+            <Marker
+              coordinate={initialCoordinates}
+              onPress={handleNavigateToDetail}
+            >
+              <View style={styles.mapMarkerContainer}>
+                <Image source={imageSource} style={styles.mapMarkerImage} />
+
+                <Text style={styles.mapMarkerTitle}>Mercado</Text>
+              </View>
+              <Icon name="arrow-down" size={20} style={styles.iconArrowDown} />
+            </Marker>
+          </MapView>
+        </View>
+      </View>
+
+      <View style={styles.itemsContainer}>
+        <ScrollView
+          contentContainerStyle={styles.scrollView}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        >
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri
+              width={42}
+              height={42}
+              uri="http://192.168.1.114:3333/uploads/lampadas.svg"
+            />
+
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri
+              width={42}
+              height={42}
+              uri="http://192.168.1.114:3333/uploads/lampadas.svg"
+            />
+
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri
+              width={42}
+              height={42}
+              uri="http://192.168.1.114:3333/uploads/lampadas.svg"
+            />
+
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri
+              width={42}
+              height={42}
+              uri="http://192.168.1.114:3333/uploads/lampadas.svg"
+            />
+
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri
+              width={42}
+              height={42}
+              uri="http://192.168.1.114:3333/uploads/lampadas.svg"
+            />
+
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri
+              width={42}
+              height={42}
+              uri="http://192.168.1.114:3333/uploads/lampadas.svg"
+            />
+
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 32,
+    paddingTop: 20 + Constants.statusBarHeight,
+  },
+
+  title: {
+    fontSize: 20,
+    fontFamily: "Ubuntu_700Bold",
+    marginTop: 24,
+  },
+
+  description: {
+    color: "#6C6C80",
+    fontSize: 16,
+    marginTop: 4,
+    fontFamily: "Roboto_400Regular",
+  },
+
+  mapContainer: {
+    flex: 1,
+    width: "100%",
+    borderRadius: 10,
+    overflow: "hidden",
+    marginTop: 16,
+  },
+
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+
+  mapMarker: {
+    width: 90,
+    height: 80,
+  },
+
+  mapMarkerContainer: {
+    width: 90,
+    height: 70,
+    backgroundColor: "#34CB79",
+    flexDirection: "column",
+    borderRadius: 8,
+    overflow: "hidden",
+    alignItems: "center",
+  },
+
+  mapMarkerImage: {
+    width: 90,
+    height: 45,
+    resizeMode: "cover",
+  },
+
+  mapMarkerTitle: {
+    flex: 1,
+    fontFamily: "Roboto_400Regular",
+    color: "#FFF",
+    fontSize: 13,
+    lineHeight: 23,
+  },
+
+  itemsContainer: {
+    flexDirection: "row",
+    marginTop: 16,
+    marginBottom: 32,
+  },
+
+  item: {
+    backgroundColor: "#fff",
+    borderWidth: 2,
+    borderColor: "#eee",
+    height: 120,
+    width: 120,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    marginRight: 8,
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    textAlign: "center",
+  },
+
+  selectedItem: {
+    borderColor: "#34CB79",
+    borderWidth: 2,
+  },
+
+  itemTitle: {
+    fontFamily: "Roboto_400Regular",
+    textAlign: "center",
+    fontSize: 13,
+  },
+
+  scrollView: { paddingHorizontal: 20 },
+
+  iconArrowDown: {
+    alignSelf: "center",
+    color: "#34CB79",
+  },
+});
 
 export default Points;
