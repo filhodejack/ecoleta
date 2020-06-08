afa552cf27f92487c5d5b38e08af2007d518e34b
Jackson Elzo <7309450+filhodejack@users.noreply.github.com>
2020-06-07 23:20:56 -0300

    feat: validate form data

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Joi } from 'celebrate'
 import express from 'express'
 import multer from 'multer'
 import multerConfig from './config/multer'
@@ -10,7 +11,28 @@ const pointsController = new PointsController()
 const upload = multer(multerConfig)
 
 routes.get('/items', itemsController.index)
-routes.post('/points', upload.single('image'), pointsController.create)
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        whatsapp: Joi.number().required(),
+        lat: Joi.number().required(),
+        long: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  pointsController.create
+)
 routes.get('/points', pointsController.index)
 routes.get('/points/:id', pointsController.show)
 
